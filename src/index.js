e5d7aa4333d2fcb8cab21513e7c85ddad695f1f1
joan-kii <joan.sb81@gmail.com>
2022-04-14 05:28:48 +0200

    cambia a browserrouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import { ContextProvider } from '@utils';
@@ -9,9 +9,9 @@ const rootElement = document.getElementById('root');
 
 render(
   <ContextProvider>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </ContextProvider>,
   rootElement
 );
